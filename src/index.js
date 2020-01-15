a39c4fd03212642efdd4a9986d913cc59a71eda5
Johnny Oshika <johnnyoshika@gmail.com>
2020-01-15 09:21:02 -0800

    Create models and register it with ApolloServer's context

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import { ApolloServer } from 'apollo-server-express';
 
 import schema from './schema';
 import resolvers from './resolvers';
+import models from './models';
 
 const app = express();
 app.use(cors());
 
 const server = new ApolloServer({
   typeDefs: schema,
-  resolvers
+  resolvers,
+  context: {
+    models
+  }
 });
 
 server.applyMiddleware({ app, path: '/graphql' });
---
+++ src/models/index.js
@@ -0,0 +1,14 @@
+let channels = {
+  1: {
+    id: '1',
+    name: 'soccer'
+  },
+  2: {
+    id: '2',
+    name: 'basketball'
+  }
+};
+
+export default {
+  channels
+};
