ee9df1546a1780f7d906344663894c3c04aa8954
Johnny Oshika <johnnyoshika@gmail.com>
2020-01-15 12:04:30 -0800

    Add addChannel mutation

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,5 +1,17 @@
+import uuidv4 from 'uuid/v4';
+
 export default {
   Query: {
     channels: (_, __, { models }) => Object.values(models.channels)
+  },
+  Mutation: {
+    addChannel: (root, { name }, { models }) => {
+      const channel = {
+        id: uuidv4(),
+        name: name
+      };
+      models.channels[channel.id] = channel;
+      return channel;
+    }
   }
 };
--- src/schema/index.js
+++ src/schema/index.js
@@ -5,6 +5,10 @@ export default gql`
     channels: [Channel!]
   }
 
+  type Mutation {
+    addChannel(name: String!): Channel
+  }
+
   type Channel {
     id: ID!
     name: String
